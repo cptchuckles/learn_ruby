6c46cf76cf07b9382b5d04dcfffc1bbd07e3b63f
cptchuckles <image1996@gmail.com>
2019-02-06 00:25:04 -0600

    Calculator solution

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add x, y
+	x + y
+end
+
+def subtract x, y
+	x - y
+end
+
+def sum array
+	array.sum
+end
+
+def mul *args
+	product = 1
+	args.each do |x|
+		product *= x
+	end
+	product
+end
+
+def pow x, y
+	x**y
+end
+
+def fac x
+	return 1 if x < 2
+	return x * fac(x-1)
+end
+
