a0df6f811f38a8b0f6969f127c0cd0420cb124c6
cptchuckles <image1996@gmail.com>
2019-02-06 14:56:24 -0600

    Add pig latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
-#write your code here
+def translate sentence
+	pl_sen = sentence.split.map! do |word|
+		word = word.downcase
+		i = 0
+		i += 1 until "aeiou".include?(word[i])
+		i -= 1 unless word.slice(0..i).include? "qu"
+		head = word.slice!(0..i)
+		word += head + "ay"
+		word
+	end
+	return pl_sen.join(' ')
+end
+
