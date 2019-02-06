e4e4736d2cbc56cd32309b1ad385d4056cb439e3
cptchuckles <image1996@gmail.com>
2019-02-06 14:56:46 -0600

    Add timer solution

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+	def initialize
+		@sec = 0
+		@min = 0
+		@hr = 0
+	end
+	def seconds
+		@sec
+	end
+	def seconds=(new_sec)
+		@hr = (new_sec / 3600).to_i % 24
+		@min = (new_sec / 60).to_i % 60
+		@sec = new_sec % 60
+	end
+	def time_string
+		"#{0 if @hr<10}#{@hr}:#{0 if @min < 10}#{@min}:#{0 if @sec < 10}#{@sec}"
+	end
 end
+
