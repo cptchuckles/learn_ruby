e0191fd731ec0bf025c83f27e5a7b6ff4aee6d0c
cptchuckles <image1996@gmail.com>
2019-02-06 14:56:36 -0600

    Add book titles solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+	def title
+		@title
+	end
+	def title=(new_title)
+		@title = new_title.downcase.split.map.with_index{|word, i|
+			word.capitalize! unless "a an and or but the of in from under over between".split.include?(word) and i > 0
+			word
+		}.join(' ')
+	end
 end
+
